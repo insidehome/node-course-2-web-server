672f86bb79cbb61c991091ac0b348580aac93f13
chao.be.thin@gmail.com <chao.be.thin@gmail.com>
2018-07-20 16:10:39 +0800

    first version

---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const hbs = require('hbs');
+
+var app = express();
+
+//register partials template
+hbs.registerPartials(__dirname + '/views/partials');
+//set view engine
+app.set('view engine', 'hbs');
+//static html files
+app.use(express.static(__dirname + '/public'));
+//register helper (function)--currentYEar
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear()
+});
+
+app.get('/', (req, res) => {
+  // // res.send('<h1>Hello Express!</h1>');
+  // res.send({
+  //   name: 'Chih Chao',
+  //   likes: [
+  //     'Riding',
+  //     'Biking',
+  //     'Baseball'
+  //   ]
+  // });
+  res.render('home.hbs', {
+    pageTitle: 'About Page',
+    userName: 'Chih Chao'
+  });
+});
+
+app.get('/about', (req, res) => {
+  // res.send('About Page');
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  });
+});
+
+// /bad - send back json with errorMessage
+app.get('/bad', (req, res) => {
+  res.send({
+    errorcode: 404,
+    errerMessage:'Unable to find this page.'
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server is up no port 3000!');
+});
