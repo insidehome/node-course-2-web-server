83b38c7f3b1262c8802ace12f6ef8de1dbf4d7ee
chao.be.thin@gmail.com <chao.be.thin@gmail.com>
2018-07-23 13:34:27 +0800

    Setup start script and heroku port

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
+
+const port = process.env.PORT || 3000;
+
 
 var app = express();
 
@@ -45,6 +49,6 @@ app.get('/bad', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('Server is up no port 3000!');
+app.listen(port, () => {
+  console.log(`Server is up no port ${port}!`);
 });
